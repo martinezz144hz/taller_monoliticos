c967707c05e7702279021a02a10355537171125c
Sofia-Murcia <samurcia@uniboyaca.edu.co>
2026-04-22 16:35:08 +0000

    Prueba de arreglo

--- models/sprint.php
+++ models/sprint.php
@@ -1,15 +1,11 @@
 <?php
-class Sprint {
-    public $id;
-    public $nombre;
-    public $fecha_inicio;
-    public $fecha_fin;
+    class Sprint {
+        private $conn;
+        private $table = "sprints";
 
-    public function __construct($id, $nombre, $fecha_inicio, $fecha_fin) {
-        $this->id = $id;
-        $this->nombre = $nombre;
-        $this->fecha_inicio = $fecha_inicio;
-        $this->fecha_fin = $fecha_fin;
+        public function __construct($db) {
+            $this->conn = $db;
+        }
+        
     }
-}
 ?>
