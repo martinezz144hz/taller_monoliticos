370b359c72f05bfe053b44e43c46606c8a27c3d8
martinezz144hz <mamartinezc@uniboyaca.edu.co>
2026-05-08 19:07:18 -0500

    ajustes en el diseño

--- index/index.php
+++ index/index.php
@@ -1,21 +1,24 @@
-<?php
-// index.php
-require_once __DIR__ . '/../config/db.php';
-?>
-
+<!-- index.php -->
 <!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <title>Registro de Retrospectivas</title>
+    <title>Registro de Retrospectivas Scrum</title>
     <link rel="stylesheet" href="../css/styles.css">
 </head>
 <body>
-    <h1>Registro de Retrospectivas Scrum</h1>
+    <!-- Encabezado azul -->
+    <header>
+        <h1>Registro de Retrospectivas Scrum</h1>
+    </header>
 
-    <ul>
-        <li><a href="../views/nueva_retro.php">Crear nueva retrospectiva</a></li>
-        <li><a href="../views/listar_retros.php">Ver retrospectivas anteriores</a></li>
-        <li><a href="../views/sprints.php">Gestionar sprints</a></li>
-    </ul>
+    <!-- Contenido gris con botones azules -->
+    <main>
+        <div class="botones">
+            <a href="../views/nueva_retro.php" class="btn">Crear nueva retrospectiva</a>
+            <a href="../views/listar_retros.php" class="btn">Ver retrospectivas anteriores</a>
+            <a href="../views/sprints.php" class="btn">Gestionar sprints</a>
+        </div>
+    </main>
 </body>
+</html>
